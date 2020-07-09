67195e315321cbcf2b6230ebe314fccd2a743dd8
fractalbonita <kathrin.korhammer@gmail.com>
2020-07-09 23:13:37 +0200

    Add GET and POST request

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import bodyParser from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const PORT = 3001
+const project = express()
+
+project.use(cors())
+
+project.use(bodyParser.json())
+project.use(bodyParser.urlencoded())
+
+mongoose.connect('mongodb://localhost:27017/capstone-project', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => console.log('Mongoose is working'))
+
+
+project.get('/plays', (request, response) => {
+  Play.find({})
+    .then((data) => response.json(data))
+    .catch((error) => {
+      console.log(error)
+      response.send(400)
+    })
+})
+
+shop.post('/plays', (request, response) => {
+  const newPlay = request.body
+
+  Play.create(newPlay)
+    .then((data) => response.json(data))
+    .catch((error) => {
+      console.log(error)
+      response.send(400)
+    })
+})
+
+project.listen(PORT, console.log(`Server is listening on PORT ${PORT}`))
