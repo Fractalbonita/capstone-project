947a78e344911421bd5de0d8c4478f1f0a192f0f
fractalbonita <kathrin.korhammer@gmail.com>
2020-07-23 02:31:02 +0200

    Add play details to Storybook

---
+++ frontend/src/domains/plays/PlayDetails/PlayDetails.stories.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import PlayDetails from '../PlayDetails'
+
+export default {
+  title: 'PlayDetails',
+  component: PlayDetails,
+}
+
+export const playDetails = () => <PlayDetails />
+
+
