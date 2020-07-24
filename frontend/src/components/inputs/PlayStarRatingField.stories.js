f99d740626182ae4de2a5826bbf2ab50054ebbe8
fractalbonita <kathrin.korhammer@gmail.com>
2020-07-24 13:54:47 +0200

    Create folder for StarRating component

---
+++ frontend/src/components/inputs/PlayStarRatingField.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import PlayStarRatingField from '../components/PlayStarRatingField'
+
+export default {
+  title: 'PlayStarRatingField',
+  component: PlayStarRatingField,
+}
+
+export const playRating = () => <PlayStarRatingField />
