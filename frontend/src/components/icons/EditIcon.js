fd64496bae67f32fe8ef78a78fcced1aba580730
fractalbonita <kathrin.korhammer@gmail.com>
2020-07-27 16:17:00 +0200

    Add edit button

---
+++ frontend/src/components/icons/EditIcon.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default () => (
+  <StyledContainer>
+    <StyledArrow
+      className="material-icons"
+      title="Edit">
+        edit
+    </StyledArrow>
+  </StyledContainer>
+)
+
+const StyledArrow = styled.span`
+  color: var(--text-color);
+  font-weight: 600;
+`
+
+const StyledContainer = styled.div`
+  align-items: center;
+  background-color: var(--surface);
+  border-radius: 50%;
+  box-shadow: -3px -3px 7px var(--inner-shadow-light-opaque), 3px 3px 7px var(--inner-shadow-dark-opaque);
+  display: flex;
+  height: 36px;
+  justify-content: center;
+  transition: all 0.2s ease-in-out;
+  width: 36px;
+
+  &:active {
+    box-shadow: inset -1px -1px 2px var(--inner-shadow-light-opaque), inset 1px 1px 2px var(--inner-shadow-dark-opaque);  }
+`
