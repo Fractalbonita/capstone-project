b38e143c942a0802f02d301a4aca8d1fd9ec0774
fractalbonita <kathrin.korhammer@gmail.com>
2020-07-29 20:38:33 +0200

    Update test description

--- frontend/src/components/inputs/PlayingTimeField.test.js
+++ frontend/src/components/inputs/PlayingTimeField.test.js
@@ -6,18 +6,18 @@ import { Formik } from 'formik'
 import PlayingTimeField from './PlayingTimeField'
 
 describe('PlayingTimeField', () => {
-  test('should render the label "Game Title *"', () => {
+  test('should render the label "Playing Time (min)"', () => {
     const { container } = render(<Formik><PlayingTimeField name={name} /></Formik>)
     expect(getByLabelText(container, 'Playing Time (min)')).toBeInTheDocument()
   })
 
-  test('should render the input field of type number', () => {
+  test('should render the input field of type text', () => {
     const { container } = render(<Formik><PlayingTimeField name={name} /></Formik>)
     expect(getByLabelText(container, 'Playing Time (min)', container).tagName).toBe('INPUT')
     expect(getByRole(container, 'textbox')).toBeInTheDocument()
   })
 
-  test('should render the placeholder "e.g. Terra Mystica"', () => {
+  test('should render the placeholder "e.g. 30"', () => {
     const { container } = render(<Formik><PlayingTimeField name={name} /></Formik>)
     expect(getByPlaceholderText(container, 'e.g. 30'))
   })
