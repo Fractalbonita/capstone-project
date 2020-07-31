f9d5238c39fbb3dc4533b6c71f1a97b57696e06b
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-01 01:42:01 +0200

    Redirect to play details instead of play collection

--- frontend/src/domains/plays/AddPlayForm/AddPlayForm.js
+++ frontend/src/domains/plays/AddPlayForm/AddPlayForm.js
@@ -25,53 +25,57 @@ const AddPlaySchema = Yup.object().shape({
 
 export default function AddPlayForm() {
   let isSubmitted = false 
+  let id = ''
 
-  return <div>
-    <StyledLink to="/log">
-      <ArrowBackIcon />
-    </StyledLink>
-    <h1>Add a New Play to your Timeline</h1>
-    <p>* Required</p>
+  return (
+    <div>
+      <StyledLink to="/log">
+        <ArrowBackIcon />
+      </StyledLink>
+      <h1>Add a New Play to your Timeline</h1>
+      <p>* Required</p>
 
-    <Formik
-      initialValues={{
-        playImage: '',
-        gameTitle: '',
-        playDate: '',
-        players: [{name: ''}],
-        playingTime: '',
-        playRating: ''
-      }}
-      initialTouched={{
-        'players[0]name': true
-      }}
-      validateOnChange
-      validationSchema={AddPlaySchema}
-      onSubmit={(values, { setSubmitting }) => {
-        uploadImage(values.playImage)
-          .then(imageURL => uploadGameData(values, imageURL))
-          .finally(() => {
-            isSubmitted = true
-            setSubmitting(false)
-          })
-      }}
-    >
-      {({ isSubmitting, setFieldValue, values }) => isSubmitted
-        ? <Redirect to="/log" />
-        : (
-        <StyledForm>
-          <PlayImageField name="playImage"
-            updateImageHandler={file => setFieldValue('playImage', file)} />
-          <GameTitleField name="gameTitle" />
-          <PlayDateField name="playDate" />
-          <PlayersField name="players" players={values.players} />
-          <PlayingTimeField name="playingTime" />
-          <PlayStarRatingField name="playRating" />
-          <PrimaryButton type="submit" disabled={isSubmitting}>Add Play</PrimaryButton>
-        </StyledForm>
-      )}
-        </Formik>
-  </div>
+      <Formik
+        initialValues={{
+          playImage: '',
+          gameTitle: '',
+          playDate: '',
+          players: [{ name: '', fraction: '', score: '', rank: '' }],
+          playingTime: '',
+          playRating: ''
+        }}
+        initialTouched={{
+          'players[0]name': true
+        }}
+        validateOnChange
+        validationSchema={AddPlaySchema}
+        onSubmit={(values, { setSubmitting }) => {
+          uploadImage(values.playImage)
+            .then(imageURL => uploadGameData(values, imageURL))
+            .then(play => id = play._id)
+            .finally(() => {
+              isSubmitted = true
+              setSubmitting(false)
+            })
+        }}
+      >
+        {({ isSubmitting, setFieldValue, values }) => isSubmitted
+          ? <Redirect to={`/log/${id}`} />
+          : (
+          <StyledForm>
+            <PlayImageField name="playImage"
+              updateImageHandler={file => setFieldValue('playImage', file)} />
+            <GameTitleField name="gameTitle" />
+            <PlayDateField name="playDate" />
+            <PlayersField name="players" players={values.players} />
+            <PlayingTimeField name="playingTime" />
+            <PlayStarRatingField name="playRating" />
+            <PrimaryButton type="submit" disabled={isSubmitting}>Add Play</PrimaryButton>
+          </StyledForm>
+        )}
+      </Formik>
+    </div>
+  )
 }
 
 const StyledLink = styled(Link)`
