1d68f1f281fafac29150f9d54bf0c63777637d23
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-02 20:47:31 +0200

    Update ternary operator alignment

--- frontend/src/domains/plays/PlayerRow/PlayerRow.js
+++ frontend/src/domains/plays/PlayerRow/PlayerRow.js
@@ -4,9 +4,18 @@ export default ({ player }) => {
   return (
     <tr>
       <td>{player.name}</td>
-      <td>{player.fraction != null ? player.fraction : '-/-'}</td>
-      <td className="number">{player.score != null ? player.score : '-/-'}</td>
-      <td className="number">{player.rank != null ? player.rank : '-/-'}</td>
+      <td>{player.fraction != null
+        ? player.fraction
+        : '-/-'}
+      </td>
+      <td className="number">{player.score != null
+        ? player.score
+        : '-/-'}
+      </td>
+      <td className="number">{player.rank != null
+        ? player.rank
+        : '-/-'}
+      </td>
     </tr>
   )
 }
