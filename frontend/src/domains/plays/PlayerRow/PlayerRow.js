b7f401a59c9205e80ca343b1670d93b3873c82a8
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-02 18:25:03 +0200

    Create PlayerRow component

---
+++ frontend/src/domains/plays/PlayerRow/PlayerRow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default ({ player }) => {
+  return (
+    <tr>
+      <td>{player.name}</td>
+      <td>{player.fraction != null ? player.fraction : '-/-'}</td>
+      <td className="number">{player.score != null ? player.score : '-/-'}</td>
+      <td className="number">{player.rank != null ? player.rank : '-/-'}</td>
+    </tr>
+  )
+}
