bbfcec1c45a348d1b11acdc4c42400a965ac3130
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-03 14:41:48 +0200

    Add story

---
+++ frontend/src/domains/plays/PlayerRow/PlayerRow.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import PlayerRow from './PlayerRow/'
+
+export default {
+  title: 'PlayerRow',
+  component: PlayerRow,
+}
+
+export const playerRow = () => <PlayerRow player={player} />
