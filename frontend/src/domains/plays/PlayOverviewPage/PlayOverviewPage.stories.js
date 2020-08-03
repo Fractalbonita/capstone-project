88ea8600478f84475702e9ff4db00766501a9194
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-03 14:47:30 +0200

    Add story

---
+++ frontend/src/domains/plays/PlayOverviewPage/PlayOverviewPage.stories.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import PlayOverviewPage from './PlayOverviewPage'
+
+export default {
+  title: 'PlayOverviewPage',
+  component: PlayOverviewPage,
+}
+
+export const playOverviewPage = () => <PlayOverviewPage />
