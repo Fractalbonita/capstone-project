4abd71eb0429e17def62d199ffaf1113014f0b1e
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-03 14:22:10 +0200

    Add additional input properties

--- frontend/src/domains/plays/PlayerEditRow/PlayerEditRow.js
+++ frontend/src/domains/plays/PlayerEditRow/PlayerEditRow.js
@@ -1,22 +1,42 @@
 import React from 'react'
 
-import PlayerEditCell from '../PlayerEditCell/PlayerEditCell'
+import PlayerEditCell from '../../../components/inputs/Input'
 
 export default ({ player, onChange }) => {
   return (
     <tr>
-      <PlayerEditCell 
-        value={player.name}
-        onChange={name => onChange({ ...player, name })} />
-      <PlayerEditCell
-        value={player.fraction}
-        onChange={fraction => onChange({ ...player, fraction })} />
-      <PlayerEditCell
-        value={player.score}
-        onChange={score => onChange({ ...player, score })} />
-      <PlayerEditCell
-        value={player.rank}
-        onChange={rank => onChange({ ...player, rank })} />
+      <td>
+        <PlayerEditCell 
+          id="name"
+          name="name"
+          type="text"
+          value={player.name}
+          onChange={name => onChange({ ...player, name })} />
+      </td>
+        <td>
+        <PlayerEditCell
+          id="fraction"
+          name="fraction"
+          type="text"
+          value={player.fraction}
+          onChange={fraction => onChange({ ...player, fraction })} />
+        </td>
+      <td>
+        <PlayerEditCell
+          id="score"
+          name="score"
+          type="text"
+          value={player.score}
+          onChange={score => onChange({ ...player, score })} />
+      </td>
+      <td>
+        <PlayerEditCell
+          id="rank"
+          name="rank"
+          type="text"
+          value={player.rank}
+          onChange={rank => onChange({ ...player, rank })} />
+      </td>
     </tr>
   )
 }
