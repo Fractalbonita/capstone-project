fa432e5e1c46a3044b7ec4d6cce75a119d62a1b1
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-05 18:42:32 +0200

    Delete comment

--- frontend/src/domains/games/GameOverviewPage/GameOverviewPage.js
+++ frontend/src/domains/games/GameOverviewPage/GameOverviewPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
-//import styled from 'styled-components'
 
 import gameData from '../../../data/gameData.json'
 import GameDetails from '../GameDetails/GameDetails'
@@ -9,7 +8,7 @@ import ArrowBackIcon from '../../../components/icons/ArrowBackIcon'
 export default function GameOverviewPage() {
   let { id } = useParams()
   const game = gameData.find(item => item._id === id)
-  
+
   return (
     <>
       <Link to="/games"> 
