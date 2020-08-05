aa21b993ee635fd24fdf78b688b0035dd490965c
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-05 16:15:39 +0200

    Add props and styling

---
+++ frontend/src/components/FeatureList.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default ({ title, list, ...props }) => (
+  <>
+  <h3>{title}</h3>
+  <StyledList>
+    {list && list.map((field, index) => (
+      <li key={index} {...props}>{field}</li>
+    ))}
+  </StyledList>
+  </>
+)
+
+const StyledList = styled.ul`
+list-style: none;
+padding: 0;
+
+li {
+  background-color: var(--primary);
+  border: 1px solid var(--primary);
+  border-radius: 20px;
+  box-shadow: -3px -3px 7px #fff, 3px 3px 7px var(--inner-shadow-dark-opaque);
+  color: var(--surface);
+  display: inline-block;
+  font-family: var(--body);
+  font-size: 14px;
+  letter-spacing: 0.05rem;
+  margin: 7px;
+  padding: 5px 10px;
+}
+`
