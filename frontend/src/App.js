9b31239b45bebaca44e1046644f424991cfe07aa
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-05 12:15:11 +0200

    Add WelcomePage to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Redirect, Route, Switch } from 'react-router-dom'
 
 import AddPlayForm from './domains/plays/AddPlayForm/AddPlayForm'
-import PlayTimelinePage from './domains/plays/PlayTimelinePage/PlayTimelinePage'
+import GameOverViewPage from './domains/games/GameOverviewPage/GameOverviewPage'
+import GamesPage from './domains/games/GamesPage/GamesPage'
 import PlayOverviewPage from './domains/plays/PlayOverviewPage/PlayOverviewPage'
+import PlayTimelinePage from './domains/plays/PlayTimelinePage/PlayTimelinePage'
+import WelcomePage from './domains/welcome/WelcomePage'
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route exact path="/" component={WelcomePage} />
+        <Route exact path="/games" component={GamesPage} />
+        <Route exact path="/games/:id" component={GameOverViewPage} />
         <Route exact path="/log" component={PlayTimelinePage} />
         <Route exact path="/log/addplay" component={AddPlayForm} />
         <Route path="/log/:id" component={PlayOverviewPage} />
