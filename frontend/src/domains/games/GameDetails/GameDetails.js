c4858a571a8b91a49074dc5e75a9cc73bd1414dc
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-05 18:43:17 +0200

    Add styling

--- frontend/src/domains/games/GameDetails/GameDetails.js
+++ frontend/src/domains/games/GameDetails/GameDetails.js
@@ -1,7 +1,9 @@
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 
 import Icon from '../../../utilities/Icon'
+import FeatureList from '../../../components/FeatureList'
+import KeyFeature from '../../../components/KeyFeature'
 
 export default ({ game }) => {
   return (
@@ -11,37 +13,36 @@ export default ({ game }) => {
       <Icon type="outlined_flag" title="Played" />
       <p>{game.averageRating}</p>
       <h1>{game.gameTitle}</h1>
-      <h2>{game.year}</h2>
-      <KeyFeature icon="group" title="Players" feature={game.players} />
-      <KeyFeature icon="psychology" title="Age" feature={game.age} />
-      <KeyFeature icon="access_time" title="Playing Time" feature={game.playingTime} />
-      <h3>Publisher</h3>
+      <h3>{game.year}</h3>
+      <StyledKeyFeatureContainer>
+        <KeyFeature icon="group" title="Players" feature={game.players} />
+        <KeyFeature icon="psychology" title="Age" feature={game.age} />
+        <KeyFeature icon="access_time" title="Playing Time (min)" feature={game.playingTime} />
+      </StyledKeyFeatureContainer>
+      <h2>Publisher</h2>
       <p>{game.publisher}</p>
-      <h3>Designer</h3>
+      <h2>Designer</h2>
       <p>{game.designer}</p>
-      <FeatureList title="Type" list={game.type}/>
+      <FeatureList title="Type" list={game.type} />
       <FeatureList title="Category" list={game.category} />
       <FeatureList title="Mechanisms" list={game.mechanisms} />
-      <p>{game.description}</p>
+      <h2>Description</h2>
+      <StyledDescription>{game.description}</StyledDescription>
     </>
   )
 }
 
-const KeyFeature = ({ icon, title, feature }) => (
-  <div>
-    <Icon type={icon} alt="" />
-    <p>{title}</p>
-    <p>{feature}</p>
-  </div>
-)
+const StyledKeyFeatureContainer = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  margin: 25px 0;
+  text-align: center;
+`
 
-const FeatureList = ({ title, list }) => (
-  <>
-    <h3>{title}</h3>
-    <ul>
-      {list && list.map((field, index) => (
-        <li key={index}>{field}</li>
-      ))}
-    </ul>
-  </>
-)
+const StyledDescription = styled.p`
+  font-display: block;
+  line-height: 1.5;
+  word-break: normal;
+`
