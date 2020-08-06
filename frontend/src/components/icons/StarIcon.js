9794d69de1446c1215c64f6f64c4f3e0f888e52b
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-06 16:22:07 +0200

    Add ...props

--- frontend/src/components/icons/StarIcon.js
+++ frontend/src/components/icons/StarIcon.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
 
-export default ({ isSelected = true, onClick }) => (
+export default ({ isSelected = true, ...props }) => (
     <StyledStar className={
       isSelected
         ? "material-icons star-highlighted"
         : "material-icons star"
-    } onClick={onClick}>
+    } {...props}>
       star
     </StyledStar>
 )
 
 const StyledStar = styled.span`
-cursor: pointer;
+  cursor: pointer;
   font-size: 24px;
   letter-spacing: 0.2rem;
   text-shadow: 1px 1px 2px var(--shadow);
