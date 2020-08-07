bcc80a497f57fcc2cef16d42d053f3ecb9f7d88e
fractalbonita <kathrin.korhammer@gmail.com>
2020-08-07 19:21:55 +0200

    Setup client for games

---
+++ frontend/src/services/gameClient.js
@@ -0,0 +1,26 @@
+import gameData from '../data/gameData.json'
+import { gamesOnWishlist, gamesAlreadyPlayed } from './userClient'
+
+export function fetchGames() {
+  return Promise.resolve(gameData)
+}
+
+export function fetchGamesOnWishlist() {
+  return Promise.all([fetchGames(), gamesOnWishlist()])
+    .then(([games, wishlist]) => {
+      return games.filter(({ _id }) => wishlist.includes(_id))
+    })
+}
+
+export function fetchPlayedGames() {
+  return Promise.all([fetchGames(), gamesAlreadyPlayed()])
+    .then(([games, played]) => {
+      return games.filter(({ _id }) => played.includes(_id))
+    })
+}
+
+export function fetchGameDetails(id) {
+  const game = gameData.find(item => item._id === id)
+  return Promise.resolve(game)
+}
+
