897ac477d8f01d5b14f83ec7747c5c7ce196fb34
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-26 00:05:07 +0200

    Add proptypes to playdatefield input

--- frontend/src/components/inputs/PlayDateField.js
+++ frontend/src/components/inputs/PlayDateField.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import * as Yup from 'yup'
+import PropTypes from 'prop-types'
 
 import ErrorMessage from '../../styles/StyledErrorMessage'
 import StyledField from '../../styles/StyledField'
 import StyledLabel from '../../styles/StyledLabel'
 
-export default ({ name }) => (
-  <>
-    <StyledLabel htmlFor={name}>Date *</StyledLabel>
-    <StyledField name={name} id={name} type="date" />
-    <ErrorMessage name={name} component="div" />
-  </>
-)
+PlayDateField.propTypes = {
+  name: PropTypes.string,
+}
+
+export default function PlayDateField({ name }) {
+  return (
+    <>
+      <StyledLabel htmlFor={name}>Date *</StyledLabel>
+      <StyledField name={name} id={name} type="date" />
+      <ErrorMessage name={name} component="div" />
+    </>
+  )
+}
 
 export const PlayDateFieldValidator = Yup.date()
   .min(new Date(1900, 0, 1))
