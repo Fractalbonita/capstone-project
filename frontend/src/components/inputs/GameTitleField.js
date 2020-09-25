9cf937aa502181457c3e4b95fac0c38b09948cee
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-25 23:58:24 +0200

    Add proptypes to gametitlefield input

--- frontend/src/components/inputs/GameTitleField.js
+++ frontend/src/components/inputs/GameTitleField.js
@@ -1,22 +1,29 @@
 import React from 'react'
 import * as Yup from 'yup'
+import PropTypes from 'prop-types'
 
 import ErrorMessage from '../../styles/StyledErrorMessage'
 import StyledField from '../../styles/StyledField'
 import StyledLabel from '../../styles/StyledLabel'
 
-export default ({ name }) => (
-  <>
-    <StyledLabel htmlFor={name}>Game Title *</StyledLabel>
-    <StyledField
-      name={name}
-      id={name}
-      type="text"
-      placeholder="e.g. Terra Mystica"
-    />
-    <ErrorMessage name={name} component="div" />
-  </>
-)
+GameTitleField.propTypes = {
+  name: PropTypes.string,
+}
+
+export default function GameTitleField({ name }) {
+  return (
+    <>
+      <StyledLabel htmlFor={name}>Game Title *</StyledLabel>
+      <StyledField
+        name={name}
+        id={name}
+        type="text"
+        placeholder="e.g. Terra Mystica"
+      />
+      <ErrorMessage name={name} component="div" />
+    </>
+  )
+}
 
 export const GameTitleFieldValidator = Yup.string()
   .max(100, `The title of the game is too long.`)
