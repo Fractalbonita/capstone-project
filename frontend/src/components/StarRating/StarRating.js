cb850268f066118c3f3dc8a7c4937b24ad316d27
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-26 00:53:06 +0200

    Add proptyes to starrating component

--- frontend/src/components/StarRating/StarRating.js
+++ frontend/src/components/StarRating/StarRating.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import Star from '../icons/StarIcon'
 
-export default ({ numberOfStars = 10, selectedStars, handleRating }) => (
-  <div>
-    {[...Array(numberOfStars).keys()].map(i => (
-      <Star
-        key={i}
-        isSelected={i < selectedStars}
-        onClick={() => handleRating(i + 1)}
-      />
-    ))}
-  </div>
-)
+StarRating.propTypes = {
+  numberOfStars: PropTypes.number,
+  selectedStars: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  handleRating: PropTypes.func,
+}
+
+export default function StarRating({
+  numberOfStars = 10,
+  selectedStars,
+  handleRating,
+}) {
+  return (
+    <div>
+      {[...Array(numberOfStars).keys()].map(i => (
+        <Star
+          key={i}
+          isSelected={i < selectedStars}
+          onClick={() => handleRating(i + 1)}
+        />
+      ))}
+    </div>
+  )
+}
