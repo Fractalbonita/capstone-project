d36a5d250ca452373f3a87ac682fbd5867dd6d1c
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-25 23:22:50 +0200

    Add missing dependency

--- frontend/src/domains/games/GameDetails/GameDetails.js
+++ frontend/src/domains/games/GameDetails/GameDetails.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
-import { isOnWishlist, isAlreadyPlayed, toggleOnWishlist, toggleOnPlayedGamesList } from '../../../services/userClient'
+import {
+  isOnWishlist,
+  isAlreadyPlayed,
+  toggleOnWishlist,
+  toggleOnPlayedGamesList,
+} from '../../../services/userClient'
 import Icon from '../../../utilities/Icon'
 import FeatureList from '../../../components/FeatureList'
 import KeyFeature from '../../../components/KeyFeature'
@@ -13,18 +18,24 @@ export default ({ game }) => {
   useEffect(() => {
     isAlreadyPlayed(game._id).then(setAlreadyPlayed)
     isOnWishlist(game._id).then(setOnWishlist)
-  }, [])
+  }, [game._id])
 
   return (
     <>
       <StyledIcons>
-        <Icon id="wishlist-icon"
-          type={onWishlist ? "favorite" : "favorite_border"} 
-          onClick={() => toggleOnWishlist(game._id).then(setOnWishlist)} />
-        <Icon id="played-icon" 
-          type={alreadyPlayed ? "flag" : "outlined_flag"} 
-          onClick={() => toggleOnPlayedGamesList(game._id).then(setAlreadyPlayed)} />
-        </StyledIcons>
+        <Icon
+          id="wishlist-icon"
+          type={onWishlist ? 'favorite' : 'favorite_border'}
+          onClick={() => toggleOnWishlist(game._id).then(setOnWishlist)}
+        />
+        <Icon
+          id="played-icon"
+          type={alreadyPlayed ? 'flag' : 'outlined_flag'}
+          onClick={() =>
+            toggleOnPlayedGamesList(game._id).then(setAlreadyPlayed)
+          }
+        />
+      </StyledIcons>
       <StyledImage>
         <img src={game.imageURL} alt="Board game cover" />
         <BggRatingStar rating={game.averageRating} />
@@ -36,7 +47,11 @@ export default ({ game }) => {
       <StyledKeyFeatureContainer>
         <KeyFeature icon="group" title="Players" feature={game.players} />
         <KeyFeature icon="psychology" title="Age" feature={game.age} />
-        <KeyFeature icon="access_time" title="Playing Time (min)" feature={game.playingTime} />
+        <KeyFeature
+          icon="access_time"
+          title="Playing Time (min)"
+          feature={game.playingTime}
+        />
       </StyledKeyFeatureContainer>
       <h2>Publisher</h2>
       <p>{game.publisher}</p>
@@ -46,15 +61,21 @@ export default ({ game }) => {
       <FeatureList title="Category" list={game.category} />
       <FeatureList title="Mechanism" list={game.mechanisms} />
       <h2>Description</h2>
-      {game.description.split('\n').map((paragraph, index) =>
+      {game.description.split('\n').map((paragraph, index) => (
         <StyledDescription key={index}>{paragraph}</StyledDescription>
-      )}
+      ))}
     </>
   )
 }
 
 const BggRatingStar = ({ rating, size = 64 }) => (
-  <StyledBggRatingStar style={{ width: size + 'px', height: size+'px', marginTop:(-5/8*size)+'px'}}>
+  <StyledBggRatingStar
+    style={{
+      width: size + 'px',
+      height: size + 'px',
+      marginTop: (-5 / 8) * size + 'px',
+    }}
+  >
     <Icon type="star" style={{ fontSize: size }} />
     <span>{rating}</span>
   </StyledBggRatingStar>
@@ -70,15 +91,15 @@ const StyledIcons = styled.div`
     text-shadow: 1px 1px 2px var(--inner-shadow-dark-opaque);
     top: 10px;
     z-index: 1;
-  
-  &#wishlist-icon {
-    left: 18px;
-  }
 
-  &#played-icon {
-    right: 18px;
+    &#wishlist-icon {
+      left: 18px;
+    }
+
+    &#played-icon {
+      right: 18px;
+    }
   }
-}
 `
 
 const StyledBggRatingStar = styled.div`
@@ -103,8 +124,8 @@ const StyledBggRatingStar = styled.div`
 `
 
 const StyledImage = styled.div`
-margin-top: 50px;
-max-width: 100vw;
+  margin-top: 50px;
+  max-width: 100vw;
 
   img {
     width: 100%;
@@ -112,7 +133,8 @@ max-width: 100vw;
 `
 
 const StyledHeader = styled.div`
-  h1, h3 {
+  h1,
+  h3 {
     text-align: center;
   }
 
