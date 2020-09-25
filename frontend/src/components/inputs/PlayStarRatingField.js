b4351910fe73a02724dd089ee3958d9b6bda9525
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-26 00:41:36 +0200

    Add proptyes to PlayStarRatingField input

--- frontend/src/components/inputs/PlayStarRatingField.js
+++ frontend/src/components/inputs/PlayStarRatingField.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 import { Field } from 'formik'
 import StyledLabel from '../../styles/StyledLabel'
 import StarRating from '../StarRating/StarRating'
 
-export default ({ name }) => (
-  <>
-    <StyledLabel htmlFor={name}>Rate the Play</StyledLabel>
-    <Field name={name} id={name} type="number">
-      {({ field: { value }, form: { setFieldValue } }) => (
-        <>
-          <StarRating
-            selectedStars={value}
-            handleRating={number => setFieldValue(name, number)}
-          />
-          <StyledNumber>{value}</StyledNumber>
-        </>
-      )}
-    </Field>
-  </>
-)
+PlayStarRatingField.propTypes = {
+  name: PropTypes.string,
+}
+
+export default function PlayStarRatingField({ name }) {
+  return (
+    <>
+      <StyledLabel htmlFor={name}>Rate the Play</StyledLabel>
+      <Field name={name} id={name} type="number">
+        {({ field: { value }, form: { setFieldValue } }) => (
+          <>
+            <StarRating
+              selectedStars={value}
+              handleRating={number => setFieldValue(name, number)}
+            />
+            <StyledNumber>{value}</StyledNumber>
+          </>
+        )}
+      </Field>
+    </>
+  )
+}
 
 const StyledNumber = styled.span`
   font-family: 'Karla', sans-serif;
