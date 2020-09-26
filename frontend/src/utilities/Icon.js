b3d784ea1afd950ed2c30fb8b30866d75959894e
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-26 16:51:37 +0200

    Add proptyes to icon component

--- frontend/src/utilities/Icon.js
+++ frontend/src/utilities/Icon.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default ({ type, ...props }) => (
-  <span className="material-icons" {...props}>
-    {type}
-  </span>
-)
+Icon.propTypes = {
+  type: PropTypes.string,
+}
+
+export default function Icon({ type, ...props }) {
+  return (
+    <span className="material-icons" {...props}>
+      {type}
+    </span>
+  )
+}
