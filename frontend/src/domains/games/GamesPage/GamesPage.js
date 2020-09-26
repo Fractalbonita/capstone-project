84fdedfc7ecd46cb656a924ff0a8b021168cc52c
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-26 15:12:37 +0200

    Relocate filterlink component

--- frontend/src/domains/games/GamesPage/GamesPage.js
+++ frontend/src/domains/games/GamesPage/GamesPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import styled from 'styled-components'
+
+import FilterLink from './FilterLink'
 
 import {
   fetchGames,
@@ -40,46 +42,7 @@ export default function GamesPage() {
   )
 }
 
-const FilterLink = ({ filter, title, search }) => (
-  <StyledLink
-    to={'/games' + filter}
-    className={search === filter ? 'active' : ''}
-  >
-    <span>{title}</span>
-  </StyledLink>
-)
-
 const StyledFilter = styled.div`
   display: grid;
   grid-template-columns: 12ch 10ch 8ch;
-`
-
-const StyledLink = styled(Link)`
-  -webkit-tap-highlight-color: transparent;
-  color: var(--text-color);
-  font-family: var(--button-font);
-  margin-bottom: 1.5rem;
-  outline: none;
-  padding-top: 15px;
-  text-decoration: none;
-
-  span {
-    border-bottom: 2px solid transparent;
-  }
-
-  &:hover {
-    color: var(--text-color);
-
-    span {
-      border-color: var(--text-color);
-    }
-  }
-
-  &.active {
-    color: var(--primary);
-
-    span {
-      border-color: var(--primary);
-    }
-  }
 `
