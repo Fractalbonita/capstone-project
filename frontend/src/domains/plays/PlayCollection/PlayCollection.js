447409f0d2a0b91c66496499b012a6da53178d49
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-26 16:53:56 +0200

    Add proptyes to playcollection component

--- frontend/src/domains/plays/PlayCollection/PlayCollection.js
+++ frontend/src/domains/plays/PlayCollection/PlayCollection.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 import Play from '../Play/Play'
+
+PlayCollection.propTypes = {
+  plays: PropTypes.array,
+}
 
 export default function PlayCollection({ plays }) {
   return (
