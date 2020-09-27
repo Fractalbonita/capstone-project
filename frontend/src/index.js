ca0ffeaa21c1b6e6d50fdb73840d2b27b4a17533
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-09-27 15:22:02 +0200

    Apply scrolltotop component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
 import GlobalStyles from './styles/GlobalStyles'
 import ReactDOM from 'react-dom'
+import ScrollToTop from './utilities/ScrollToTop'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <GlobalStyles />
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>,
