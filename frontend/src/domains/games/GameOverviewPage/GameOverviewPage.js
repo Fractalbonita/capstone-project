ee318f0174a28578aace09898d4a7035c1bc2a61
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-10-16 00:01:04 +0200

    Fix navigation link position

--- frontend/src/domains/games/GameOverviewPage/GameOverviewPage.js
+++ frontend/src/domains/games/GameOverviewPage/GameOverviewPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
+import styled from 'styled-components'
 
 import gameData from '../../../data/gameData.json'
 import GameDetails from '../GameDetails/GameDetails'
@@ -11,10 +12,15 @@ export default function GameOverviewPage() {
 
   return (
     <>
-      <Link to="/games">
+      <StyledLink to="/games">
         <ArrowBackIcon />
-      </Link>
+      </StyledLink>
       <GameDetails game={game} />
     </>
   )
 }
+
+const StyledLink = styled(Link)`
+  position: relative;
+  z-index: 100;
+`
