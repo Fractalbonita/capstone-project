40ce7225bf039762b6663ffc311a88498e742b8b
Kathrin Korhammer <kathrin.korhammer@gmail.com>
2020-10-15 02:14:11 +0200

    dropdownsort component and sorting functions for plays to playtimelinepage

--- frontend/src/domains/plays/PlayTimelinePage/PlayTimelinePage.js
+++ frontend/src/domains/plays/PlayTimelinePage/PlayTimelinePage.js
@@ -5,8 +5,21 @@ import styled from 'styled-components'
 import { fetchPlays } from '../../../services/playsClient'
 import AddPlayIcon from '../../../components/icons/AddPlayIcon'
 import PlayCollection from '../PlayCollection/PlayCollection'
+import DropDownSort from '../../../components/DropDownSort'
 
 export default function PlayTimelinePage() {
+  const sortingCriteria = {
+    'Name: A-Z': (a, b) =>
+      a.gameTitle.toLowerCase() < b.gameTitle.toLowerCase() ? 1 : 0,
+    'Name: Z-A': (a, b) =>
+      a.gameTitle.toLowerCase() > b.gameTitle.toLowerCase() ? 1 : 0,
+    Date: (a, b) => (a.playDate > b.playDate ? 1 : 0),
+    Rating: (a, b) => (a.playRating > b.playRating ? 1 : 0),
+  }
+
+  const sortingCriteriaNames = Object.keys(sortingCriteria)
+
+  const [selectedCriterion, setSelectedCriterion] = useState('Date')
   const [plays, setPlays] = useState([])
 
   useEffect(() => {
@@ -16,8 +29,15 @@ export default function PlayTimelinePage() {
   return (
     <>
       <h1>Plays</h1>
+      <DropDownSort
+        sortingCriteria={sortingCriteriaNames}
+        selectedCriterion={selectedCriterion}
+        onChange={sorting => setSelectedCriterion(sorting)}
+      />
       {plays.length ? (
-        <PlayCollection plays={plays} />
+        <PlayCollection
+          plays={[...plays].sort(sortingCriteria[selectedCriterion])}
+        />
       ) : (
         <StyledText>
           There are no plays <br /> on your timeline yet. <br />
